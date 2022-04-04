74d99531952c0b7691587dd3faf82386b3d176c2
lyahoff <lyahoff.vlad@gmail.com>
2022-04-04 16:58:22 +0600

    done with morse scale

--- lib/morse.dart
+++ lib/morse.dart
@@ -57,148 +57,229 @@ class _MorseLogicState extends State<MorseLogic> {
     return ListView(
       padding: EdgeInsets.all(10),
       children: [
-        header('Падал ли пациент в последние 3 мес?'),
-        RadioListTile(
-            title: Text('Да'),
-            value: _val1,
-            groupValue: 25,
-            onChanged: (val) {
-              setState(() {
-                _val1 = 25;
-              });
-            }),
-        RadioListTile(
-            title: Text('Нет'),
-            value: _val1,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val1 = 0;
-              });
-            }),
-        header('Есть ли сопутствующее заболевание?'),
-        RadioListTile(
-            title: Text('Да'),
-            value: _val2,
-            groupValue: 15,
-            onChanged: (val) {
-              setState(() {
-                _val2 = 15;
-              });
-            }),
-        RadioListTile(
-            title: Text('Нет'),
-            value: _val2,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val2 = 0;
-              });
-            }),
-        header('Самостоятельность при ходьбе'),
-        RadioListTile(
-            title: Text(
-                'Ходит сам (даже если при помощи кого-то), или строгий постельный режим, неподвижно лежит'),
-            value: _val3,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val3 = 0;
-              });
-            }),
-        RadioListTile(
-            title: Text('Костыли/ходунки/трость'),
-            value: _val3,
-            groupValue: 15,
-            onChanged: (val) {
-              setState(() {
-                _val3 = 15;
-              });
-            }),
-        RadioListTile(
-            title: Text('Опирается о мебель или стены для поддержки'),
-            value: _val3,
-            groupValue: 30,
-            onChanged: (val) {
-              setState(() {
-                _val3 = 30;
-              });
-            }),
-        header(
-            'Принимает внутривенное вливание/принимает гепарин, фраксипарин'),
-        RadioListTile(
-            title: Text('Да'),
-            value: _val4,
-            groupValue: 25,
-            onChanged: (val) {
-              setState(() {
-                _val4 = 25;
-              });
-            }),
-        RadioListTile(
-            title: Text('Нет'),
-            value: _val4,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val4 = 0;
-              });
-            }),
-        header('Походка'),
-        RadioListTile(
-            title: Text('Нормальная (ходит свободно)'),
-            value: _val5,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val5 = 0;
-              });
-            }),
-        RadioListTile(
-            title: Text(
-                'Слегка несвободная (ходит с остановками, шаги короткие, иногда с задержкой)'),
-            value: _val5,
-            groupValue: 10,
-            onChanged: (val) {
-              setState(() {
-                _val5 = 10;
-              });
-            }),
-        RadioListTile(
-            title: Text(
-                'Нарушения (не может встать, ходит опираясь, смотрит вниз)'),
-            value: _val5,
-            groupValue: 20,
-            onChanged: (val) {
-              setState(() {
-                _val5 = 20;
-              });
-            }),
-        header('Психическое состояние'),
-        RadioListTile(
-            title: Text('Знает о своей способности двигаться'),
-            value: _val6,
-            groupValue: 0,
-            onChanged: (val) {
-              setState(() {
-                _val6 = 0;
-              });
-            }),
-        RadioListTile(
-            title: Text('Не знает или забывает, что нужна помощь при движении'),
-            value: _val6,
-            groupValue: 15,
-            onChanged: (val) {
-              setState(() {
-                _val6 = 15;
-              });
-            }),
-        SizedBox(
-          height: 20,
+        Card(
+          elevation: 2,
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'Шкала падений Морзе используется для оценки вероятности риска падения, в связи с наличием наследственных или приобретенных факторов риска.',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ),
-        resultMorse(_result, _total, _val1, _val2, _val3, _val4, _val5, _val6),
-        SizedBox(
-          height: 20,
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Падал ли пациент в последние 3 мес?',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Да'),
+                value: _val1,
+                groupValue: 25,
+                onChanged: (val) {
+                  setState(() {
+                    _val1 = 25;
+                  });
+                }),
+            RadioListTile(
+                title: Text('Нет'),
+                value: _val1,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val1 = 0;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Есть ли сопутствующее заболевание?',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Да'),
+                value: _val2,
+                groupValue: 15,
+                onChanged: (val) {
+                  setState(() {
+                    _val2 = 15;
+                  });
+                }),
+            RadioListTile(
+                title: Text('Нет'),
+                value: _val2,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val2 = 0;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Самостоятельность при ходьбе',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text(
+                    'Ходит сам (даже если при помощи кого-то), или строгий постельный режим, неподвижно лежит'),
+                value: _val3,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val3 = 0;
+                  });
+                }),
+            RadioListTile(
+                title: Text('Костыли/ходунки/трость'),
+                value: _val3,
+                groupValue: 15,
+                onChanged: (val) {
+                  setState(() {
+                    _val3 = 15;
+                  });
+                }),
+            RadioListTile(
+                title: Text('Опирается о мебель или стены для поддержки'),
+                value: _val3,
+                groupValue: 30,
+                onChanged: (val) {
+                  setState(() {
+                    _val3 = 30;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Принимает внутривенное вливание/принимает гепарин, фраксипарин',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Да'),
+                value: _val4,
+                groupValue: 25,
+                onChanged: (val) {
+                  setState(() {
+                    _val4 = 25;
+                  });
+                }),
+            RadioListTile(
+                title: Text('Нет'),
+                value: _val4,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val4 = 0;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Походка',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Нормальная (ходит свободно)'),
+                value: _val5,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val5 = 0;
+                  });
+                }),
+            RadioListTile(
+                title: Text(
+                    'Слегка несвободная (ходит с остановками, шаги короткие, иногда с задержкой)'),
+                value: _val5,
+                groupValue: 10,
+                onChanged: (val) {
+                  setState(() {
+                    _val5 = 10;
+                  });
+                }),
+            RadioListTile(
+                title: Text(
+                    'Нарушения (не может встать, ходит опираясь, смотрит вниз)'),
+                value: _val5,
+                groupValue: 20,
+                onChanged: (val) {
+                  setState(() {
+                    _val5 = 20;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                'Психическое состояние',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Знает о своей способности двигаться'),
+                value: _val6,
+                groupValue: 0,
+                onChanged: (val) {
+                  setState(() {
+                    _val6 = 0;
+                  });
+                }),
+            RadioListTile(
+                title: Text(
+                    'Не знает или забывает, что нужна помощь при движении'),
+                value: _val6,
+                groupValue: 15,
+                onChanged: (val) {
+                  setState(() {
+                    _val6 = 15;
+                  });
+                }),
+          ]),
+        ),
+        Card(
+          elevation: 2,
+          child: Column(children: [
+            SizedBox(
+              height: 5,
+            ),
+            ListTile(
+              title: resultMorse(
+                  _result, _total, _val1, _val2, _val3, _val4, _val5, _val6),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+          ]),
         ),
       ],
     );
@@ -235,5 +316,7 @@ Widget resultMorse(
   }
 
   return Text(_result,
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18));
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+      ));
 }
