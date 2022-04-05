c44af0ec3dccae19b951545b51ce274f19c3017f
lyahoff <lyahoff.vlad@gmail.com>
2022-04-05 10:03:24 +0600

    widget Scaffold was refactored inside WillPopScope

--- lib/barthel.dart
+++ lib/barthel.dart
@@ -7,29 +7,36 @@ class Barthel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(
-            'Индекс Бартел',
-            style: TextStyle(color: Colors.black),
+      home: WillPopScope(
+        onWillPop: () async {
+          runApp(Rehabilitology());
+          return false;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(
+              'Индекс Бартел',
+              style: TextStyle(color: Colors.black),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  color: Colors.black,
+                  onPressed: () {
+                    runApp(Rehabilitology());
+                  },
+                  tooltip:
+                      MaterialLocalizations.of(context).openAppDrawerTooltip,
+                );
+              },
+            ),
           ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.arrow_back),
-                color: Colors.black,
-                onPressed: () {
-                  runApp(Rehabilitology());
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
+          body: BarthelRadio(),
         ),
-        body: BarthelRadio(),
       ),
     );
   }
